6fdff5114554eb95443bbe019eb17c717af0ceca
Jon-Louis Dalton <jldalton12@gmail.com>
2022-04-07 21:40:44 +0200

    Added deposit notifications

--- functions/firebase/firebase.py
+++ functions/firebase/firebase.py
@@ -3,6 +3,7 @@ import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import messaging
 
 # <========== Initialize firebase ==========>
 cred = credentials.Certificate('functions/firebase/firebaseKey.json')
@@ -13,15 +14,26 @@ db = firestore.client()
 def MakeDeposit(uniqueID, amount):
 
     transaction = db.transaction()
-    ProcessDeposit(transaction, uniqueID, amount)
+    ProcessDeposit(transaction, uniqueID, amount, SendDepositNotification)
 
 
 @firestore.transactional
-def ProcessDeposit(transaction, uniqueID, amount):
+def ProcessDeposit(transaction, uniqueID, amount, callback):
 
     # Read data
     snapshot = db.collection(u'users').where(
         u'depositID', u'==', uniqueID).get(transaction=transaction)
+
+    # Record the user notification tokens
+    notificationTokens = snapshot[0].to_dict()["notificationTokens"]
+
+    # Record the user's notifications
+    notifications = snapshot[0].to_dict()["notifications"]
+    notifications.append(
+        'Deposit: Incoming deposit of R{}'.format(amount))
+
+    # Record the user's ID
+    userID = snapshot[0].id
 
     # Record the deposit
     transaction.set(db.collection(u'deposits').document(), {
@@ -32,5 +44,40 @@ def ProcessDeposit(transaction, uniqueID, amount):
 
     # Update the user's balance
     transaction.update(db.collection(u'users').document(snapshot[0].id), {
-        u'balance': snapshot[0].to_dict()["balance"] + amount
+        u'balance': snapshot[0].to_dict()["balance"] + amount,
+        u'notifications': notifications
     })
+
+    callback(amount, notificationTokens, notifications, userID)
+
+
+def SendDepositNotification(amount, notificationTokens, notifications, userID):
+
+    for token in notificationTokens:
+
+        try:
+
+            message = messaging.Message(
+                notification=messaging.Notification(
+                    title='Deposit',
+                    body='Incoming deposit of R{}'.format(amount)
+                ),
+                android=messaging.AndroidConfig(
+                    priority='high',
+                    notification=messaging.AndroidNotification(
+                        sound='default'),
+                ),
+                apns=messaging.APNSConfig(
+                    payload=messaging.APNSPayload(
+                        aps=messaging.Aps(sound='default'),
+                    ),
+                ),
+                token=token)
+
+            response = messaging.send(message)
+
+            print('Successfully sent message:', response)
+
+        except Exception:
+
+            print("Token not found")
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import functions.firebase.firebase as Firebase
 
 # Execute this module dirrectly
 if __name__ == "__main__":
-    Firebase.MakeDeposit("WUR3JZGN", 600)
+    Firebase.MakeDeposit("WUR3JZGN", 1000)
